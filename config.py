1e73d40008aba3d06c8b80484090d6bcae7aa207
killfish <31091732+iuky08@users.noreply.github.com>
2022-08-28 22:16:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,14 @@ import os
 from dotenv import load_dotenc
 from urllib3 import disable_warnings
 
-dotenv_path = os.path.join(os.path.dirname(__file__), .env)
-if os.path.exists(dotenv_path, verbose = True)
+@pytest.fixture(autouse=True, scope="session")
+def load_env():
+    """Загружает всё из файла .env и подставляет в environment variables.
+       Запускается автоматически один раз для всех тестов.
+       """
+    dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
+    if os.path.exists(dotenv_path):
+        load_dotenv(dotenv_path, verbose=True)
 
 config = dict()
 config['URL'] = os.getenv('URL', False)
