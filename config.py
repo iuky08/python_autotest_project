552d7f76fe614036d86e3ac3c8a3f569df1e7b1f
killfish <31091732+iuky08@users.noreply.github.com>
2022-08-28 22:17:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,12 @@ import os
 from dotenv import load_dotenc
 from urllib3 import disable_warnings
 
+config = dict()
+config['URL'] = os.getenv('URL', False)
+config['AUTH_TOKEN'] = os.getenv('AUTH_TOKEN', False)
+config['USERNAME'] = os.getenv('USERNAME', False)
+config['PASSWORD'] = os.getenv('PASSWORD', False)
+
 @pytest.fixture(autouse=True, scope="session")
 def load_env():
     """Загружает всё из файла .env и подставляет в environment variables.
@@ -11,11 +17,12 @@ def load_env():
     dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
     if os.path.exists(dotenv_path):
         load_dotenv(dotenv_path, verbose=True)
+        
+@pytest.fixture(scope="session")
+def setup() -> SetupHelper:
+    """Создаёт объект, помогающий настроить окружение перед началом теста.
+       Используйте его в отдельных тестах или в подготовительных фикстурах."""
+    return SetupHelper(ApiClient(os.getenv('URL')), ApiClient(os.getenv('AUTH_TOKEN')))
 
-config = dict()
-config['URL'] = os.getenv('URL', False)
-config['AUTH_TOKEN'] = os.getenv('AUTH_TOKEN', False)
-config['USERNAME'] = os.getenv('USERNAME', False)
-config['PASSWORD'] = os.getenv('PASSWORD', False)
 
 disable_warnings()
